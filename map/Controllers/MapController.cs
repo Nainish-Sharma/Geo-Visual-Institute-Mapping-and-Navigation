117639006bb6985cfad0c9dcff34d998125b26ee
naini <naini@LAPTOP-2BAJ9HGC>
2025-08-29 12:07:21 +0530

    Add project files.

---
+++ map/Controllers/MapController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.Mvc;
+using map.Models;
+
+namespace map.Controllers
+{
+    public class MapController : Controller
+    {
+        public ActionResult Index()
+        {
+            var locations = GetInstitutesFromDatabase();
+            return View(locations);
+        }
+
+        private List<InstituteLocation> GetInstitutesFromDatabase()
+        {
+            List<InstituteLocation> list = new List<InstituteLocation>();
+            string connStr = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT InstID, InstName, InstAddress, CityID, Latitude, Longitude,accCount FROM accrInst ", conn);
+
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new InstituteLocation
+                        {
+                            InstID = Convert.ToInt32(reader["InstID"]),
+                            InstName = reader["InstName"].ToString(),
+                            InstAddress = reader["InstAddress"].ToString(),
+                            CityID = reader["CityID"].ToString(),
+                            Latitude = Convert.ToDouble(reader["Latitude"]),
+                            Longitude = Convert.ToDouble(reader["Longitude"]),
+                            AccCount = Convert.ToInt32(reader["accCount"])
+                        });
+
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ map/Models/InstituteLocation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace map.Models
+{
+    public class InstituteLocation
+    {
+        public int InstID { get; set; }
+
+        public int AccCount { get; set; }
+        public string InstName { get; set; }
+        public string InstAddress { get; set; }
+        public string CityID { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
